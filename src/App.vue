f91409c2c4da7204a2e6393de2ce158b01c03786
Bobby Westberg <bobby.westbergaA@frontra.no>
2019-10-08 12:20:32 +0200

    First InfoBox

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <InfoBox
+        type="warning"
+        group="The heading's heading"
+        heading="Yolo"
+        body="Lorem ipsum doler sit amet"
+      />
+      <InfoBox
+        type="info"
+        group="Group"
+        heading="This is an important message"
+        body="Lorem ipsum doler sit amet"
+      />
+      <HelloWorld msg="This be an elegant message"/>
+    </main>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import InfoBox from './components/InfoBox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    InfoBox
   }
 }
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 60px 0 0 0;
+  background-color: #8e98a3;
+}
+main {
+  box-shadow: 0 0 10px 10px #2c3e5057;
+  border-radius: 8px;
+  max-width: 80%;
+  margin: 0 auto;
+  background-color: white;
 }
 </style>
---
+++ src/components/InfoBox.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :class="{
+    [$style.root]: true,
+    [$style.warning]: isWarning
+  }">
+    <header>
+      <p><small>{{ group }}</small></p>
+      <h3>{{ heading }}</h3>
+    </header>
+    <p>{{ body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoBox',
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+    group: {
+      type: String,
+      default: "Some topic",
+    },
+    heading: String,
+    body: String,
+  },
+  computed: {
+    isWarning() {
+      return this.type === 'warning'
+    }
+  }
+}
+</script>
+
+<style module>
+  .root {
+    box-shadow:
+      inset 0px 11px 8px -10px #000,
+      inset 0px -11px 8px -10px #000;
+    min-height: 100px;
+    background-color: #ced3d8;
+  }
+  .warning {
+    border: 1px solid red;
+  }
+</style>
